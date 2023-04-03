0cb144f32b624d13bc133b4a9bff58077b1aea78
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-04-03 11:26:22 -0300

    Feat: Added page routes system

--- frontend/src/template/menu.jsx
+++ frontend/src/template/menu.jsx
@@ -11,7 +11,7 @@ export default props => {
                 </div>
                 <div id='navbar' className='navbar-collapse collapse'>
                     <ul className="nav navbar-nav">
-                        <li><a href="#/todos">Tarefas</a></li>
+                        <li><a href="#/todos">Tarefas {console.log(1)}</a></li>
                         <li><a href="#/about">Sobre</a></li>
                     </ul>
                 </div>
