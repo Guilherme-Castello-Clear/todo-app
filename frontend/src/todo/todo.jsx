47a04030c145fe81601183e390b9c68173724412
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-04-04 10:45:41 -0300

    Feat: Integrated Create and Delete from DB

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -15,8 +15,22 @@ export default class Todo extends Component{
         this.state = {description: '', list: []}
         
         this.handleAdd = this.handleAdd.bind(this)
-
+        this.handleRemove = this.handleRemove.bind(this)
         this.handleChange = this.handleChange.bind(this)
+
+        this.refresh()
+    }
+
+    refresh(){
+        axios.get(`${URL}?sort=-createdAt`)
+            .then(resp => this.setState({...this.state, description: '', list: resp.data}))
+        
+        console.log(this.state.list)
+    }
+
+    handleRemove(todo){
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
     }
 
     handleChange(e){
@@ -26,10 +40,7 @@ export default class Todo extends Component{
     handleAdd(){
         const description = this.state.description
         axios.post(URL, { description })
-        .then( resp => console.log('WORKED'))
-        .catch(e =>{
-            console.log(e)
-        })
+        .then( resp => this.refresh())
     }
 
 
@@ -38,7 +49,7 @@ export default class Todo extends Component{
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'/>
                 <TodoForm description={this.state.description} handleChange={this.handleChange} handleAdd={this.handleAdd}/>
-                <TodoList/>
+                <TodoList list={this.state.list} handleRemove={this.handleRemove}/>
             </div>
         
             )
