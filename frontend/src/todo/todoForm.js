4cbfe34fd86362e87bf2f51d8620e0a9d7cbc3aa
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-04-06 09:45:50 -0300

    Feat: Using Action Creator to change description

--- frontend/src/todo/todoForm.js
+++ frontend/src/todo/todoForm.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
 
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
-
+import { changeDescription } from './todoActions'
 
 const TodoForm = props => {
 
@@ -20,7 +22,7 @@ const TodoForm = props => {
         <div role='form' className='todoForm'>
     
             <Grid cols='12 9 10'>
-                <input value={props.description} onChange={props.handleChange} id='description' className='form-control' placeholder='Adicione uma tarefa' onKeyUp={keyHandler}/>
+                <input value={props.description} onChange={props.changeDescription} id='description' className='form-control' placeholder='Adicione uma tarefa' onKeyUp={keyHandler}/>
             </Grid>
             <Grid cols='12 3 2'>
                 <IconButton style='primary' icon='plus' onClick={props.handleAdd}></IconButton>
@@ -32,4 +34,6 @@ const TodoForm = props => {
 }
 
 const mapStateToProps = state => ({description: state.todo.description})
-export default connect(mapStateToProps)(TodoForm)
+
+const mapDispatchToProps = dispatch => bindActionCreators({changeDescription}, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
---
+++ frontend/src/todo/todoReducer.js
@@ -0,0 +1,27 @@
+const INITIAL_STATE = {
+    description: 'Ler livro',
+    list: [{
+        _id: 1,
+        description: 'Pagar Fatura',
+        done: true
+    },
+    {
+        _id: 2,
+        description: 'Reunião',
+        done: false 
+    },
+    {
+        _id: 3,
+        description: 'Consulta',
+        done: false 
+    }]
+}
+
+export default (state = INITIAL_STATE, action) => {
+    switch(action.type){
+        case 'DESCRIPTION_CHANGED':
+            return {...state, description: action.payload}
+        default:
+            return state
+    }
+}
