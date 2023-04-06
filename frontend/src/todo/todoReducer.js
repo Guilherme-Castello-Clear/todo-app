c0815abaf6c1245f07f503bd17aa37f875aab4d7
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-04-06 10:48:52 -0300

    Feat: Using redux-promise to improve request in database

--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,26 +1,16 @@
 const INITIAL_STATE = {
-    description: 'Ler livro',
-    list: [{
-        _id: 1,
-        description: 'Pagar Fatura',
-        done: true
-    },
-    {
-        _id: 2,
-        description: 'Reunião',
-        done: false 
-    },
-    {
-        _id: 3,
-        description: 'Consulta',
-        done: false 
-    }]
+    description: '',
+    list: []
 }
 
 export default (state = INITIAL_STATE, action) => {
     switch(action.type){
         case 'DESCRIPTION_CHANGED':
             return {...state, description: action.payload}
+        
+        case 'TODO_SEARCHED':
+            return {...state, list: action.payload.data}
+        
         default:
             return state
     }
